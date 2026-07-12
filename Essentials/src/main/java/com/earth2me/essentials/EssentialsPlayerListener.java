e8928dce7ba381979ca9207da6ba4362dddeb7dd
syelindzer <syelindzer@gmail.com>
2026-07-12 05:59:22 +0200

    onplayerrespawn: add monitor event

--- Essentials/src/main/java/com/earth2me/essentials/EssentialsPlayerListener.java
+++ Essentials/src/main/java/com/earth2me/essentials/EssentialsPlayerListener.java
@@ -10,6 +10,7 @@ import com.earth2me.essentials.textreader.TextInput;
 import com.earth2me.essentials.textreader.TextPager;
 import com.earth2me.essentials.userstorage.ModernUserMap;
 import com.earth2me.essentials.utils.CommonPlaceholders;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import com.earth2me.essentials.utils.DateUtil;
 import com.earth2me.essentials.utils.FormatUtil;
 import com.earth2me.essentials.utils.LocationUtil;
@@ -177,6 +178,7 @@ public class EssentialsPlayerListener implements Listener {
         }
     }
 
+
     @EventHandler(priority = EventPriority.NORMAL)
     public void onPlayerRespawn(final PlayerRespawnEvent event) {
         final User user = ess.getUser(event.getPlayer());
@@ -188,6 +190,10 @@ public class EssentialsPlayerListener implements Listener {
         }
     }
 
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerDeath(final org.bukkit.event.entity.PlayerDeathEvent event) {
+        event.getEntity().spigot().respawn();
+    }
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerChat(final AsyncPlayerChatEvent event) {
         final User user = ess.getUser(event.getPlayer());
